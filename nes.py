083e9e7ce6513f0de7ab316762cf42ecf15d7053
kavitha-kave <60390429+kavitha-kave@users.noreply.github.com>
2020-01-29 15:08:23 +0530

    Add files via upload

---
+++ nes.py
@@ -0,0 +1,16 @@
+b= int(input("enter the first nmber:"))
+c= int(input("enter the second nmber:"))
+i=1
+while i<10:
+    d=b+c
+    i += 1
+   
+print(d)
+a = input("do u want to continue(y/n)")
+if a=="y":
+   d=b+c
+   print(d)
+elif a == "n":
+  i += 9 
+
+         
